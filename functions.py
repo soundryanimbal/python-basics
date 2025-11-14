9aec4d40bcb09ae2d5352ca4cf4b6e5f7e773209
soundryanimbal <soundryanimbal@gmail.com>
2025-11-14 19:51:37 +0530

    Implement add and greet functions
    Added basic functions for addition and greeting.

---
+++ functions.py
@@ -0,0 +1,11 @@
+# Basic function examples
+
+def add(a, b):
+    return a + b
+
+def greet(name):
+    return f"Hello, {name}!"
+
+# Testing the functions
+print(add(5, 3))
+print(greet("Soundrya"))
